78844070a015aca04602743b76199ea70f830cb3
devmutinda <davidkymutinda@gmail.com>
2022-11-02 05:17:06 +0000

    Modified code

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,23 +5,27 @@ This module queries reddit API
 import requests
 
 
-def recurse(subreddit, hot_list=[], count=0):
+def recurse(subreddit, hot_list=[], after="", count=0):
     """
     Returns a list containing titles of all hot articles for a given subreddit
     """
-    if not hot_list:
-        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-        headers = {"User-agent": "Linux-david"}
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
-            return None
-        subreddit = response.json().get('data').get('children')
-        # print(response.json().get('data').get('children')[1].get('data').get('id'))
-        hot_list.append(subreddit[count].get('data').get('title'))
-        return recurse(subreddit, hot_list, count + 1)
-    if count >= len(subreddit):
-        # print(hot_list)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-agent": "Linux-david"}
+    params = {
+            "after": after,
+            "count": count,
+            "limit": 100
+            }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code != 200:
+        return None
+    results = response.json().get('data')
+    after = results.get('after')
+    count += results.get('dist')
+    for c in results.get('children'):
+        hot_list.append(c.get('data').get('title'))
+
+    if after:
         return hot_list
-    else:
-        hot_list.append(subreddit[count].get('data').get('title'))
-        return recurse(subreddit, hot_list, count + 1)
+    return recurse(subreddit, hot_list, after, count)
